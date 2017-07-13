d1d3e19205b48af7054b50d6f10e38e3fa3a7b6f
Renato Bueno da Silva <renato@MacBook-Pro-de-Renato.local>
2017-07-13 11:24:49 -0300

    Encontrando corpo do xml para posteriormente interpretar

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('pam');
+var http = require('https');
+var parseString = require('xml2js').parseString;
+
+
+var host = 'https://webservice.s7.exacttarget.com/etframework.wsdl';
+
+// host = 'http://correiosapi.apphb.com/cep/17012190'
+
+http.get(host, (res) => {
+        // console.log(res);
+        var body = '';
+
+        res.on('data', (retorno) => {
+            body += retorno;
+        })
+
+        res.on('end', function() {
+            parseString(body, function (err, result) {
+                // corpo do xml
+                console.log(result.definitions.types[0].schema[0]);
+            });
+        });
+    })
+
